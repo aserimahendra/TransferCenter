68293f4cd01da11a59bbb55fde9816505ea26785
rohitmishra <rkmishra6300@outlook.com>
2025-10-19 01:20:36 +0530

    updated sql schema for auditlog

--- TransferCenterDB/Tables/auditLog.sql
+++ TransferCenterDB/Tables/auditLog.sql
@@ -1,13 +1,13 @@
 CREATE TABLE AuditLog (
                           Id BIGINT IDENTITY(1,1) PRIMARY KEY,         -- Auto-increment primary key
-                          UserId INT NOT NULL,                         -- User performing the action
-                          ControllerName NVARCHAR(100) NOT NULL,       -- Controller or Module name
-                          ActionName NVARCHAR(100) NOT NULL,           -- Specific method or API endpoint
-                          ActionType NVARCHAR(50) NULL,                -- Optional: Create / Update / Delete / View
-                          RequestData NVARCHAR(MAX) NULL,              -- Input data (as JSON)
-                          ResponseData NVARCHAR(MAX) NULL,             -- Output or response data
-                          ExecutionStatus NVARCHAR(20) NULL,           -- Success / Failed / Exception
+                          UserId varchar(100) NOT NULL,                         -- User performing the action
+                          ControllerName VARCHAR(100) NOT NULL,       -- Controller or Module name
+                          ActionName VARCHAR(100) NOT NULL,           -- Specific method or API endpoint
+                          ActionType VARCHAR(50) NULL,                -- Optional: Create / Update / Delete / View
+                          RequestData VARCHAR(MAX) NULL,              -- Input data (as JSON)
+                          ResponseData VARCHAR(MAX) NULL,             -- Output or response data
+                          ExecutionStatus VARCHAR(20) NULL,           -- Success / Failed / Exception
                           ExecutionDate DATETIME2 NOT NULL DEFAULT SYSUTCDATETIME(), -- UTC timestamp
-                          ClientIp NVARCHAR(50) NULL,                  -- IP address of request
-                          Remarks NVARCHAR(500) NULL                   -- Additional info or error details
+                          ClientIp VARCHAR(50) NULL,                  -- IP address of request
+                          Remarks VARCHAR(500) NULL                   -- Additional info or error details
 );
